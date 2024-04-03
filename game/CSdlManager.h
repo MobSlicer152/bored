07a4a353e0dfab2342d9287f70f81e1bbdc65195
MobSlicer152 <brambleclaw1414@gmail.com>
2024-04-03 11:14:04 -0400

    Slightly improve hacky code

--- game/CSdlManager.h
+++ game/CSdlManager.h
@@ -13,7 +13,7 @@ struct CSdlManager
 
     // TODO: idOffset is a terrible idea, but I guess it should be fine cause the ABI is mostly stable
     void GetEvents(std::vector<SDL_Event> &events, u32 id, SDL_EventType first = SDL_EVENT_WINDOW_FIRST,
-                   SDL_EventType last = SDL_EVENT_WINDOW_LAST, u64 idOffset = 16);
+                   SDL_EventType last = SDL_EVENT_WINDOW_LAST, u64 idOffset = offsetof(SDL_WindowEvent, windowID));
 
     CSdlManager(const CSdlManager &other) = delete;
     void operator=(const CSdlManager &Other) = delete;
